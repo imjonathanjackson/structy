5644bc339dfb6a17a19cf1094d5849a9593f3712
Jonathan Jackson <imjonathanjackson@gmail.com>
2025-01-20 15:21:46 -0800

    Update hey_programmer.py

--- hey_programmer/hey_programmer.py
+++ hey_programmer/hey_programmer.py
@@ -1,2 +1,6 @@
+# Welcome to Structy! Watch the approach video in the tab at the top of this window to get started.
+
+# Write a function greet that takes in a string argument, s, and returns the string "hey s". No tricks here. Run the tests to check your work.
+
 def greet(s):
   return "hey " + s
